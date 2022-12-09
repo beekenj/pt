fb1b41e468cc3f955f18c87425080a0938389662
Jack Beeken <beekenj@gmail.com>
2022-12-08 17:58:08 -0700

    Added a python notebook for combat simulation. Added argv for parseCSV script to make it more modular.

--- src/data/parseCSV.js
+++ src/data/parseCSV.js
@@ -1,9 +1,16 @@
 const fs = require("fs");
 const { parse } = require("csv-parse");
 
-const data = [];
+if (process.argv.slice(2).length < 2) {
+  console.log('Please include a source file and a destination file in the command line args');
+  process.exit(1);
+}
 
-fs.createReadStream("./deck.csv")
+const data = [];
+const arg1 = process.argv.slice(2)[0];
+const arg2 = process.argv.slice(2)[1];
+
+fs.createReadStream(arg1)
   .pipe(
     parse({
       delimiter: ",",
@@ -21,9 +28,9 @@ fs.createReadStream("./deck.csv")
   .on("end", function () {
     // 👇 log the result array
     console.log("parsed csv data:");
-    console.log(data);
+    // console.log(data);
     fs.writeFile(
-        'data.js', 
+        arg2, 
         `const data = ${JSON.stringify(data)}; export default data;`, 
         function (err) {
             if (err) return console.log(err);
